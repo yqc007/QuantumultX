7bd319a067bbf1c5293d2bad2511399f5c6eb047
Hausd0rff <s2232094232@gmail.com>
2021-12-22 23:02:45 +0800

    Update DisableBaiduJumpAction.js

--- Purity/DisableBaiduJumpAction.js
+++ Purity/DisableBaiduJumpAction.js
@@ -6,7 +6,7 @@
 电报频道：https://t.me/yqc_123
 问题反馈：https://t.me/yqc_007
 使用声明：⚠️此脚本仅供学习与交流，
-                    请勿转载与贩卖！⚠️⚠️⚠️
+        请勿转载与贩卖！⚠️⚠️⚠️
 
 ********************************
 
@@ -25,6 +25,6 @@ hostname = *.baidu.com
 
 var hausd0rff = $request.headers;
 
-      hausd0rff['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/16C50 Quark/604.1 T7/10.3 SearchCraft/2.6.3 (Baidu; P1 8.0.0)';
+    hausd0rff['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/16C50 Quark/604.1 T7/10.3 SearchCraft/2.6.3 (Baidu; P1 8.0.0)';
 
 $done({headers : hausd0rff});
