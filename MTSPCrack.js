169c056f30b2eb83e7fecffb31957aa60b3f5cf7
Hausd0rff <s2232094232@gmail.com>
2022-02-08 21:37:55 +0800

    Update MTSPCrack.js

--- MTSPCrack.js
+++ MTSPCrack.js
@@ -5,9 +5,9 @@
 软件版本：1.0.1
 下载地址：https://shrtm.nu/mt96
 脚本作者：Hausd0rff
-更新时间：2021.11.17
+更新时间：2022-02-07
 电报频道：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_007
+问题反馈：https://t.me/yqc_777
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
 
@@ -15,13 +15,13 @@
 
 [rewrite_local]
 
-# 蜜桃视频解锁无限次数
-\/t\w{4}\/\d\/t\/ url 307 /token/aa1dceb9b04740e2c09623342303c6ef/t/
-^https?:\/\/.*\.youbangjiazheng\.com\/api\/Video\/m3u8\/id.+ url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/MTSPCrack.js
+# > 蜜桃视频解锁无限次数
+\/t\w{4}\/\d\/t\/ url 307 /token/5d10208372cbd649de04b9731c1b55db/t/
+^https?:\/\/.*mt.*\.com\/api\/Video\/m3u8\/id.+ url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/MTSPCrack.js
 
 [mitm] 
 
-hostname = *.youbangjiazheng.com
+hostname = *mt*.com
 *
 *
 */
