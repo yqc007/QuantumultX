bcb4aa2cbbb3ed60219f4c090610ffcfd64c44ca
Hausd0rff <s2232094232@gmail.com>
2022-02-15 11:29:57 +0800

    Update Block100SVIPCrack.js

--- Block100SVIPCrack.js
+++ Block100SVIPCrack.js
@@ -2,12 +2,12 @@
  *
  *
 脚本功能：拦截100解锁超级会员
-软件版本：2.1.3
+软件版本：3.0.3
 下载地址：http://t.cn/A6MLFAJf
 脚本作者：Hausd0rff
-更新时间：2022-01-05
+更新时间：2022-02-15
 电报频道：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_007
+问题反馈：https://t.me/yqc_777
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
 
@@ -28,6 +28,6 @@ hostname = tagit.hyhuo.com
 
 var body = $response.body;
     
-    body = "lvCQG8cCxqficLk+LttK+L2YRSLM39a3jj+Mfoba4wNaV8x54RYUAWrJvcwwFZemFEWG5xK2FlI8k2hlHsSW7uaAeW45GPxSPt1uvVwPI5zLiMJuHvGycPsqS+ozuIELUsmxd901h+fpVaDUSy+5KRrW+M+mVJRARweO6FjfHosZ/pP8kkty3hz0uDRWb9enH9pG+36Issw9aJCxSkok0Q==";
+    body = "lvCQG8cCxqficLk+LttK+L2YRSLM39a3jj+Mfoba4wNaV8x54RYUAWrJvcwwFZemFEWG5xK2FlI8k2hlHsSW7uaAeW45GPxSPt1uvVwPI5zLiMJuHvGycPsqS+ozuIELZnJYBJk2unXdxQCbEof4Rmgo3m7O+4ByRFtdbPMM/COhtxmSVeHXnNcSfVlkALSnTyKl0xORwbczE2UdkNg/uioD0AtxIrGJ4jaUVNVKvJsibH0dOKL/bljPSrG31RQt";
 
 $done({ body });
