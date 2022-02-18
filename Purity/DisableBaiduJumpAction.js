86317940139d23bed60ede5f00bbc24a1be98574
Hausd0rff <s2232094232@gmail.com>
2022-02-18 17:45:16 +0800

    Update DisableBaiduJumpAction.js

--- Purity/DisableBaiduJumpAction.js
+++ Purity/DisableBaiduJumpAction.js
@@ -1,7 +1,7 @@
 /*
  *
  *
-脚本功能：百度不跳转
+脚本功能：解决Safari中疯狂跳转百度
 脚本作者：Hausd0rff
 电报频道：https://t.me/yqc_123
 问题反馈：https://t.me/yqc_007
@@ -12,7 +12,7 @@
 
 [rewrite_local]
 
-# 百度不跳转
+# > 百度不跳转
 ^https?:\/\/.*\.baidu\.com\/.+ url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/Purity/DisableBaiduJumpAction.js
 
 [mitm] 
@@ -24,7 +24,5 @@ hostname = *.baidu.com
 
 
 var hausd0rff = $request.headers;
-
-    hausd0rff['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/16C50 Quark/604.1 T7/10.3 SearchCraft/2.6.3 (Baidu; P1 8.0.0)';
-
+hausd0rff['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/16C50 Quark/604.1 T7/10.3 SearchCraft/2.6.3 (Baidu; P1 8.0.0)';
 $done({headers : hausd0rff});
