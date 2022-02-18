d5bdf7ed5296dbab8db9834e909b84625532313f
Hausd0rff <s2232094232@gmail.com>
2022-02-18 17:25:40 +0800

    Update FiMoCameraFVIPCrack.js

--- FiMoCameraFVIPCrack.js
+++ FiMoCameraFVIPCrack.js
@@ -15,7 +15,7 @@
 
 [rewrite_local]
 
-# FIMO解锁终身会员
+# > FIMO解锁终身会员
 ^https?:\/\/server\.yoyiapp\.com\/fimo-user\/apple\/certificate$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/FiMoCameraFVIPCrack.js
 
 [mitm] 
