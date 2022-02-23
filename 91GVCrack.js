e7d8b07194ded5925d78cb27ecfad0072a91ec4d
Hausd0rff <s2232094232@gmail.com>
2022-02-23 11:54:50 +0800

    Update 91GVCrack.js

--- 91GVCrack.js
+++ 91GVCrack.js
@@ -4,17 +4,16 @@
 脚本功能：91搞网站破解收费
 网站地址：https://shrtm.nu/91gv
 脚本作者：Hausd0rff
-更新时间：2021.11.15
+更新时间：2021-11-15
 电报频道：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_007
+问题反馈：https://t.me/yqc_777
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
-
-********************************
+*******************************
 
 [rewrite_local]
 
-# 91搞网站破解收费
+# > 91搞网站破解收费
 ^https?:\/\/www\.91gao1\.com\/api\/v1\/article url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/91GVCrack.js
 ^https?:\/\/www\.91gao1\.com\/api\/v1\/article url response-body is_buy":\w+ response-body is_buy":true
 
