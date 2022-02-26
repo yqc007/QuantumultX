c5512fabf89fdcfaac7aeceb9386d64ddd221989
Hausd0rff <s2232094232@gmail.com>
2022-02-26 23:40:43 +0800

    Update JavDBCrack.js

--- JavDBCrack.js
+++ JavDBCrack.js
@@ -30,7 +30,7 @@ hostname = shukego.com
 
 var hausd0rff = $request.headers;
 hausd0rff['authorization'] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MTQ0MjQ4MywidXNlcm5hbWUiOiJ5cWMwMDcifQ.EOCu_lF9TFxeZQ_2SfcwdyiU7cyBESkXiHfWBiP3k9M';
-hausd0rff['jdsignature'] = '1645887913.b1g4psngx2.920d37ae051ca15fa38a98ca57baac59';
+hausd0rff['signature'] = '45aebf2239f533611b172a8ecff036a3';
 $done({
     headers : hausd0rff
 });
