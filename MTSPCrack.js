506d3285eae9c3fdcc6ade6dbe268ee559d12750
Hausd0rff <s2232094232@gmail.com>
2022-02-26 18:35:21 +0800

    Update MTSPCrack.js

--- MTSPCrack.js
+++ MTSPCrack.js
@@ -28,9 +28,8 @@ hostname = *mt*.com
 
 
 var hausd0rff = $request.headers;
-
-    hausd0rff['X-Playback-Session-Id'] = 'E15F154E-607C-4D37-YQC6-665A098CB666';
-
-    hausd0rff['User-Agent'] = 'Flow/1.0 NetType/4g Companyid/4000';
-
-$done({headers : hausd0rff});
+hausd0rff['X-Playback-Session-Id'] = 'E15F154E-607C-4D37-YQC6-665A098CB666';
+hausd0rff['User-Agent'] = 'Flow/1.0 NetType/4g Companyid/4000';
+$done({
+    headers : hausd0rff
+});
