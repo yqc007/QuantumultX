0840a68c75a95492e6c4c99f8394412f7fcad337
Hausd0rff <s2232094232@gmail.com>
2022-02-27 09:42:42 +0800

    Update DisableBaiduJumpAction.js

--- Purity/DisableBaiduJumpAction.js
+++ Purity/DisableBaiduJumpAction.js
@@ -25,5 +25,5 @@ hostname = *.baidu.com
 var hausd0rff = $request.headers;
 hausd0rff['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/16C50 Quark/604.1 T7/10.3 SearchCraft/2.6.3 (Baidu; P1 8.0.0)';
 $done({
-    headers: hausd0rff
+    headers : hausd0rff
 });
