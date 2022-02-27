687e39966de525e5b1184c675df78f77c81b6a13
Hausd0rff <s2232094232@gmail.com>
2022-02-27 09:38:43 +0800

    Update XinYuLibraryProCrack.js

--- XinYuLibraryProCrack.js
+++ XinYuLibraryProCrack.js
@@ -5,12 +5,11 @@
 软件版本：4.2.9
 下载地址：http://t.cn/A6xkFpuX
 脚本作者：Hausd0rff
-更新时间：2021.11.15
+更新时间：2021-11-15
 电报频道：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_007
+问题反馈：https://t.me/yqc_777
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
-
 *******************************
 
 [rewrite_local]
@@ -27,10 +26,10 @@ hostname = i.xinyulib.com.cn
 
 
 var objc = JSON.parse($response.body);
-
 objc.data["vipstartTime"] = "2020-09-28";
 objc.data["vipendtime"] = "2999-09-28";
-objc.data["trueName"] = "浥轻尘𝐶𝑟𝑎𝑐𝑘";
-objc.data["headImg"] = "https://thirdqq.qlogo.cn/g?b=oidb&k=iaODNibZcQQTN0QPGShfkKIw&s=640";
-
-$done({body : JSON.stringify(objc)});
+objc.data["trueName"] = "\u6D65\u8F7B\u5C18\uD835\uDC36\uD835\uDC5F\uD835\uDC4E\uD835\uDC50\uD835\uDC58";
+objc.data["headImg"] = "https:\/\/thirdqq.qlogo.cn\/g?b=oidb&k=iaODNibZcQQTN0QPGShfkKIw&s=640";
+$done({
+    body : JSON.stringify(objc)
+});
