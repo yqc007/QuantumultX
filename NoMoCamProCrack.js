5cb9049a54cca59cad9d60ff2c53a3735c51ce8c
Hausd0rff <s2232094232@gmail.com>
2022-03-01 11:47:40 +0800

    Optimize Code

--- NoMoCamProCrack.js
+++ NoMoCamProCrack.js
@@ -7,10 +7,9 @@
 脚本作者：Hausd0rff
 更新时间：2022-01-06
 电报频道：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_007
+问题反馈：https://t.me/yqc_777
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
-
 *******************************
 
 [rewrite_local]
@@ -26,10 +25,8 @@ hostname = nomo.dafork.com
 */
 
 
-var obj = JSON.parse($response.body);
-    
-    obj.sign = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTQ2NTg1MSwiYWNjZXNzX3Rva2VuIjoiMXE3R2NLaXZITEhpbWRhOXdrNUFuYllmZElEaWxGVmxSMWpIbWhsM2I1VHc1VTlZYlh0aTMzdTJDcW1GcGpCQktNaFVBQkNnc1JjeTNWWjN0SFBwZm1VMVlvNUtXY3lLT2taUE53R2FuT0NkUnJhZWkxd3lOcHpWbG1yWUJXLXdQR3NBSmVIODNsSWxTUVlic3ljeDdPSzVFUUhsN01uWGEzMVhVdVhzSGJvREJPM2Z5RGw3YUNjTllTajZSNVZ6Y3FYTjFSRDUiLCJwcm9kdWN0cyI6W10sInByb2R1Y3RfaW9zX2lkcyI6W10sImlzX3N1YnNjcmlwdGlvbl91c2VyIjp0cnVlLCJyZWNlaXZlZF9maWxtX2lkcyI6W10sInRpbWVzdGFtcCI6MTY0Mjc4MDc1MX0.pE65QvcFOa4aGoQtFJ-pnHys8uT4MKoakGNbuCRbOy-YZ4U0UDKgqJ7G-U2m9HUMXg2m39CzVvUKwwzvUQYK-YMf0u_xFz2HIDyEGPABpuq3dv4XzRGtNORlP5qNxy0EqbyOgmVbSSNX2kacvAX7BV5mLYPJLaDR0yjPLXuB7ac9MExGw4Iejn9VCnG1bkWxvSj-gdKX0MiGJIZP4rDkRsU9_TjMHaNyg2Rdp4rVptefHUuqfV4pitjKxWbIGyKEmiCOr53lTNypDshC3R0y-NqEHlnV5Mohc5s4m0-__HXW_NwgV5f4cb8oGV25cBdHwJPT_BLtc90jrCqG7us7WA";
-
+var objc = JSON.parse($response.body);
+objc.sign = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTQ2NTg1MSwiYWNjZXNzX3Rva2VuIjoiMXE3R2NLaXZITEhpbWRhOXdrNUFuYllmZElEaWxGVmxSMWpIbWhsM2I1VHc1VTlZYlh0aTMzdTJDcW1GcGpCQktNaFVBQkNnc1JjeTNWWjN0SFBwZm1VMVlvNUtXY3lLT2taUE53R2FuT0NkUnJhZWkxd3lOcHpWbG1yWUJXLXdQR3NBSmVIODNsSWxTUVlic3ljeDdPSzVFUUhsN01uWGEzMVhVdVhzSGJvREJPM2Z5RGw3YUNjTllTajZSNVZ6Y3FYTjFSRDUiLCJwcm9kdWN0cyI6W10sInByb2R1Y3RfaW9zX2lkcyI6W10sImlzX3N1YnNjcmlwdGlvbl91c2VyIjp0cnVlLCJyZWNlaXZlZF9maWxtX2lkcyI6W10sInRpbWVzdGFtcCI6MTY0Mjc4MDc1MX0.pE65QvcFOa4aGoQtFJ-pnHys8uT4MKoakGNbuCRbOy-YZ4U0UDKgqJ7G-U2m9HUMXg2m39CzVvUKwwzvUQYK-YMf0u_xFz2HIDyEGPABpuq3dv4XzRGtNORlP5qNxy0EqbyOgmVbSSNX2kacvAX7BV5mLYPJLaDR0yjPLXuB7ac9MExGw4Iejn9VCnG1bkWxvSj-gdKX0MiGJIZP4rDkRsU9_TjMHaNyg2Rdp4rVptefHUuqfV4pitjKxWbIGyKEmiCOr53lTNypDshC3R0y-NqEHlnV5Mohc5s4m0-__HXW_NwgV5f4cb8oGV25cBdHwJPT_BLtc90jrCqG7us7WA";
 $done({
-        body : JSON.stringify(obj)
+    body : JSON.stringify(objc)
 });
