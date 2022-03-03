5e32af7fc2538fd8e610dc197e15e8ffacb1912b
Hausd0rff <s2232094232@gmail.com>
2022-03-03 23:30:13 +0800

    Update TIDALHiFiPlusCrack.js

--- TIDALHiFiPlusCrack.js
+++ TIDALHiFiPlusCrack.js
@@ -15,8 +15,7 @@
 [rewrite_local]
 
 # > TIDAL解锁HiFi Plus
-^https?:\/\/api\.tidal\.com\/v1\/(pages\/album|
-(user|track|page)s\/(\d+)\/(state|subscription|lyrics|onboarding|playbackinfopostpaywall)) url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/TIDALHiFiPlusCrack.js
+^https?:\/\/api\.tidal\.com\/v1\/(pages\/album|(user|track|page)s\/(\d+)\/(state|subscription|lyrics|onboarding|playbackinfopostpaywall)) url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/TIDALHiFiPlusCrack.js
 
 [mitm] 
 
