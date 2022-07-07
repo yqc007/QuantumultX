06e34ca32b2e87813127a8d12d8429766fc0f47c
Hausd0rff <s2232094232@gmail.com>
2022-07-07 23:55:15 +0800

    Update

--- WPSDocerVIPuserCrack.js
+++ WPSDocerVIPuserCrack.js
@@ -16,7 +16,7 @@
 ^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPowerCrack.js
 
 [mitm] 
-hostname = account.wps.cn
+hostname = *.docer.wps.cn, vipapi.wps.cn, account.wps.cn
 
 *******************************/
 
