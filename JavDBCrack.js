de6350bc5536a5781dd22ea8e6c1ea4a8e833164
Hausd0rff <s2232094232@gmail.com>
2022-09-24 23:12:58 +0800

    Update

--- JavDBCrack.js
+++ JavDBCrack.js
@@ -16,7 +16,6 @@
 [rewrite_local]
 # > JavDB解锁会员
 ^https?:\/\/.+\/api\/v1\/(movies\/.*\/play|startup) url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/JavDBCrack.js
-^https?:\/\/.+\/api\/v1\/ads$ url reject-200
 
 [mitm] 
 hostname = shukego.com, yiliang8888.com, 91mingyang.com, mnter.com, f5bc.club
