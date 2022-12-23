3b9267caaa8f441b4f1de76a46aeddcd23a0ce4d
Hausd0rff <s2232094232@gmail.com>
2022-12-23 19:41:50 +0800

    Update

---
+++ OopsExpiredProCrack.js
@@ -0,0 +1,23 @@
+/******************************
+
+脚本功能：过期啦解锁永久订阅
+软件版本：3.0.3
+下载地址：http://t.cn/A6trSJHS
+脚本作者：Passer_by_yun
+更新时间：2022-12-19
+电报频道：https://t.me/yqc_123
+问题反馈：https://t.me/yqc_777
+使用声明：⚠️仅供学习交流，🈲️商业用途
+
+*******************************
+
+[rewrite_local]
+# > 过期啦解锁永久订阅
+^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/.{36})$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/OopsExpiredProCrack.js
+
+[mitm] 
+hostname = api.revenuecat.com
+
+*******************************/
+
+function _0x2bd8(_0x206024,_0x46d8db){var _0x1e6071=_0x1e60();return _0x2bd8=function(_0x2bd89e,_0x487560){_0x2bd89e=_0x2bd89e-0x111;var _0x2a704d=_0x1e6071[_0x2bd89e];return _0x2a704d;},_0x2bd8(_0x206024,_0x46d8db);}var _0x3f9ac2=_0x2bd8;(function(_0x3ec9dc,_0xede670){var _0x586b69=_0x2bd8,_0x3eac95=_0x3ec9dc();while(!![]){try{var _0x4c44af=parseInt(_0x586b69(0x114))/0x1+parseInt(_0x586b69(0x126))/0x2+parseInt(_0x586b69(0x111))/0x3*(parseInt(_0x586b69(0x11d))/0x4)+parseInt(_0x586b69(0x117))/0x5*(parseInt(_0x586b69(0x120))/0x6)+-parseInt(_0x586b69(0x115))/0x7+-parseInt(_0x586b69(0x116))/0x8+-parseInt(_0x586b69(0x11a))/0x9;if(_0x4c44af===_0xede670)break;else _0x3eac95['push'](_0x3eac95['shift']());}catch(_0x20fc92){_0x3eac95['push'](_0x3eac95['shift']());}}}(_0x1e60,0x2363a));function _0x1e60(){var _0x14289d=['1987727WcSFwW','2053416ggTEcC','5QvOxsV','788','$RCAnonymousID:fdc1d2576ayqc666a8b0d97085684dc0','680994pFLoEa','parse','PURCHASED','53048DtSkmN','2022-11-28T10:56:00Z','com.vanemu.oops_expired.pro','986988xGcAKs','2022-11-28T10:58:32Z','2022-11-28T11:13:20Z','trial','2022-11-28T11:13:26Z','https://apps.apple.com/account/subscriptions','563506yJefIH','2022-11-28T10:56:29Z','2022-11-28T11:13:22Z','24CNsyTq','app_store','body','208911qRHltk'];_0x1e60=function(){return _0x14289d;};return _0x1e60();}var body=$response[_0x3f9ac2(0x113)],objc=JSON[_0x3f9ac2(0x11b)](body);objc={'request_date_ms':0x1840d167b40,'request_date':_0x3f9ac2(0x124),'subscriber':{'non_subscriptions':{},'first_seen':_0x3f9ac2(0x127),'original_application_version':_0x3f9ac2(0x118),'other_purchases':{},'management_url':_0x3f9ac2(0x125),'subscriptions':{'com.vanemu.oops_expired.pro':{'original_purchase_date':_0x3f9ac2(0x128),'expires_date':null,'is_sandbox':![],'refunded_at':null,'unsubscribe_detected_at':null,'grace_period_expires_date':null,'period_type':_0x3f9ac2(0x123),'purchase_date':'2022-11-28T11:13:20Z','billing_issues_detected_at':null,'ownership_type':_0x3f9ac2(0x11c),'store':_0x3f9ac2(0x112),'auto_resume_date':null}},'entitlements':{'pro':{'grace_period_expires_date':null,'purchase_date':_0x3f9ac2(0x122),'product_identifier':_0x3f9ac2(0x11f),'expires_date':null}},'original_purchase_date':_0x3f9ac2(0x11e),'original_app_user_id':_0x3f9ac2(0x119),'last_seen':_0x3f9ac2(0x121)}},body=JSON['stringify'](objc),$done({'body':body});
