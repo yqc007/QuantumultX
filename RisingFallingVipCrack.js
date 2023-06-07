92311553a8051d0ecc5a269bb42187797c48b21a
Hausd0rff <s2232094232@gmail.com>
2023-06-07 19:26:53 +0800

    Update

---
+++ RisingFallingVipCrack.js
@@ -0,0 +1,19 @@
+/******************************
+
+脚本功能：起伏冥想解锁永久会员
+应用版本：3.4
+应用下载：http://t.cn/A6pfrI3F
+脚本作者：Hausd0rff
+更新时间：2023-06-07
+脚本发布：https://t.me/yqc_123
+使用声明：‼️脚本仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+# > 起伏冥想解锁永久会员
+^https?:\/\/api\.risingfalling\.com\/api\/vip\/detail url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/RisingFallingVipCrack.js
+[mitm] 
+hostname = api.risingfalling.com
+
+*******************************/
+
+var __encode ='t.me/yqc_123',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Oxfee38=["\x50\x41\x59","\x32\x30\x30","\x73\x74\x72\x69\x6E\x67\x69\x66\x79","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];$done({body:JSON[__Oxfee38[0x2]]({"\x62\x6F\x64\x79":{"\x69\x73\x56\x69\x70":true,"\x6F\x6E\x63\x65\x46\x6F\x72\x41\x6C\x6C":true,"\x72\x65\x67\x69\x73\x74\x72\x61\x74\x69\x6F\x6E\x44\x61\x79\x73":666,"\x76\x69\x70\x54\x79\x70\x65":__Oxfee38[0x0]},"\x63\x6F\x64\x65":__Oxfee38[0x1],"\x73\x74\x61\x74\x75\x73":200})});;;(function(_0xd4d0x1,_0xd4d0x2,_0xd4d0x3,_0xd4d0x4,_0xd4d0x5,_0xd4d0x6){_0xd4d0x6= __Oxfee38[0x3];_0xd4d0x4= function(_0xd4d0x7){if( typeof alert!== _0xd4d0x6){alert(_0xd4d0x7)};if( typeof console!== _0xd4d0x6){console[__Oxfee38[0x4]](_0xd4d0x7)}};_0xd4d0x3= function(_0xd4d0x8,_0xd4d0x1){return _0xd4d0x8+ _0xd4d0x1};_0xd4d0x5= _0xd4d0x3(__Oxfee38[0x5],_0xd4d0x3(_0xd4d0x3(__Oxfee38[0x6],__Oxfee38[0x7]),__Oxfee38[0x8]));try{_0xd4d0x1= __encode;if(!( typeof _0xd4d0x1!== _0xd4d0x6&& _0xd4d0x1=== _0xd4d0x3(__Oxfee38[0x9],__Oxfee38[0xa]))){_0xd4d0x4(_0xd4d0x5)}}catch(e){_0xd4d0x4(_0xd4d0x5)}})({})
