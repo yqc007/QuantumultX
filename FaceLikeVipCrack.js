57805983562ad004ebaace86e4ac796f82423292
Hausd0rff <s2232094232@gmail.com>
2023-06-12 18:42:46 +0800

    Create

---
+++ FaceLikeVipCrack.js
@@ -0,0 +1,19 @@
+/******************************
+
+脚本功能：趣拍解锁永久会员
+应用版本：1.3.3
+应用下载：http://t.cn/A6NrtUjx
+脚本作者：Hausd0rff
+更新时间：2023-05-14
+脚本发布：https://t.me/yqc_123
+使用声明：‼️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+# > 趣拍解锁永久会员
+^https?:\/\/s\.mcwifi\.cn\/hw\/apple\/subscribe\/v1 url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/FaceLikeVipCrack.js
+[mitm] 
+hostname = s.mcwifi.cn
+
+*******************************/
+
+var _0x49640a=_0x16c6;(function(_0x2b1266,_0xa7edf9){var _0x2c070b=_0x16c6,_0x231765=_0x2b1266();while(!![]){try{var _0x764dc9=parseInt(_0x2c070b(0x15c))/0x1+-parseInt(_0x2c070b(0x15b))/0x2*(-parseInt(_0x2c070b(0x15e))/0x3)+-parseInt(_0x2c070b(0x163))/0x4+parseInt(_0x2c070b(0x156))/0x5*(-parseInt(_0x2c070b(0x160))/0x6)+-parseInt(_0x2c070b(0x159))/0x7+parseInt(_0x2c070b(0x155))/0x8+parseInt(_0x2c070b(0x157))/0x9;if(_0x764dc9===_0xa7edf9)break;else _0x231765['push'](_0x231765['shift']());}catch(_0x938a6c){_0x231765['push'](_0x231765['shift']());}}}(_0xad2f,0xe127e));var body=$response['body'],hausd0rff=JSON[_0x49640a(0x15a)](body);function _0xad2f(){var _0x2918e7=['10010336YxtHhb','451735klSaUy','639198YgzOTe','stringify','3279486rjEcQg','parse','68IjriWV','1833611dEqFbs','\x65\x78\x70\x69\x72\x65\x73\x44\x61\x74\x65\x4d\x73','54057mwjSQU','32495443200000','42iePZUY','\x69\x73\x56\x69\x70','\x64\x61\x74\x61','6981624EtAeci'];_0xad2f=function(){return _0x2918e7;};return _0xad2f();}function _0x16c6(_0x112b52,_0x1dbdd0){var _0xad2fac=_0xad2f();return _0x16c6=function(_0x16c6a6,_0x25df11){_0x16c6a6=_0x16c6a6-0x155;var _0x93c8a8=_0xad2fac[_0x16c6a6];return _0x93c8a8;},_0x16c6(_0x112b52,_0x1dbdd0);}hausd0rff[_0x49640a(0x162)][_0x49640a(0x161)]='\x31',hausd0rff[_0x49640a(0x162)][_0x49640a(0x15d)]=_0x49640a(0x15f),$done({'body':JSON[_0x49640a(0x158)](hausd0rff)});
