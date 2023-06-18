8e03eaa49b363f4a6972ef7578edf770989a8808
Hausd0rff <s2232094232@gmail.com>
2023-06-18 20:48:58 +0800

    Create

---
+++ KuaiShouPornCrack.js
@@ -0,0 +1,20 @@
+/******************************
+
+脚本功能：快手约炮解锁全部视频
+应用版本：3.1.0+50
+应用下载：https://bit.ly/43Lf6y6
+脚本作者：Hausd0rff
+更新时间：2023-06-18
+脚本发布：https://t.me/yqc_123
+使用声明：‼️仅供学习交流, 🈲️商业用途 ⚠️
+*******************************
+[rewrite_local]
+# > 快手约炮解锁全部视频
+^https?:\/\/.*\/api\/app\/user\/info$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/KuaiShouPornCrack.js
+^https?:\/\/.*\/api\/app\/media\/m3u8\/.+\.m3u8 url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/KuaiShouPronCrack.js
+[mitm] 
+hostname = woqasdfe.uwkakpqkdc.com, jgtgjf.zhdffvwsq.com, ehgewjfg.wldfnjh.com, ifbgkeowq.keuaosld.com, qwezxjcjhsdf.cyyeshb.com, jhbnvd.zhmsyaenq.com
+
+*******************************/
+
+var _0x1337da=_0x4fbc;(function(_0x18ae40,_0x90264a){var _0x5b041b=_0x4fbc,_0x66742a=_0x18ae40();while(!![]){try{var _0x327898=-parseInt(_0x5b041b(0xb8))/0x1*(-parseInt(_0x5b041b(0xbf))/0x2)+parseInt(_0x5b041b(0xc5))/0x3*(parseInt(_0x5b041b(0xb9))/0x4)+parseInt(_0x5b041b(0xc4))/0x5+parseInt(_0x5b041b(0xbc))/0x6*(-parseInt(_0x5b041b(0xba))/0x7)+parseInt(_0x5b041b(0xc1))/0x8*(-parseInt(_0x5b041b(0xb7))/0x9)+parseInt(_0x5b041b(0xbb))/0xa*(parseInt(_0x5b041b(0xbe))/0xb)+-parseInt(_0x5b041b(0xc0))/0xc;if(_0x327898===_0x90264a)break;else _0x66742a['push'](_0x66742a['shift']());}catch(_0x32ba36){_0x66742a['push'](_0x66742a['shift']());}}}(_0x1910,0xa685d));function _0x1910(){var _0x1718d9=['6711795reqcGP','858SFJjFq','9wbVOgJ','2emQKNF','860UWeHNG','7kkmgnR','10uFIczv','2603508HfwiZi','stringify','7713321NRnPST','203146KbgqiY','2857320DocmUv','7632808ryMTTQ','body','zDrMBCBZLbv7W87ryP67nHVLc+z41gT5iVc/IqsM8n7J22Uu+exg/zwlUb6qgWhYejgN9S7y+Gk4fafX56ncfmV8OTn9Dyb6m2nmcUJZKEAhep64gDclkgibGycchsLYSQXxflQkwBEiRkwhuLUGTeXScW2LxnMOkp0PFEgoBtY3T7pyvLOcp/h+zBey6THS0FtIE1YNeM+vB6uk/OwuxqnGCzQw13bIJbGzGh4TpbcF3vPQklTKbxKUUjEgDRDsZP5msED/Wi5Mwg2qwDpiMDhoACI9mF4YZUPJOsB/FW99rlj0W28is874nVHKj+MXALEFq1u9IW6TfA6IkeZxyswf/lKymAnM0BuHLqtfzuPXWDksU2JrN0TDXD8c48baA+7unIt5oSu/y2m60Hj1t9RYeoiLU+xMWP8skMW/nfxos8Lg+878T4k3xwwSg3vDnwcg2NWnmrnAPzWUG9ymUtjn/TpbNF6vPCnZQ2lKL5rjPCvqGuP5iisf2NMd8Q8ZNCshF+cHK6vgMe2V3AULQ7yFONyUqE00lfa6m9imfJvntZDKshPTuq0bF72gdKwDqhcGGiI/iGMBe11DqQqQrUyakBLVtwEvdbsTh8Az3RnTfqVT1azLijbhY2Lu5OJ+mCII860Q5v8yiesFuxwGE0007sRUHqf8ZygmNwhth71pBfmBN8QRxtyOciskxKKf9sVVyXrTpH7HpNc89kkOBo6EpAb38T09uMe7nq2eNQDzNWP4hsvLy7mzGUTlfjXcCHQtBg4tVQhu0KlrbgaOh1X1H/okszcOjE6FWyTPQUq3YpeB/uaalhBUDnSjn2oJePbtoUDujyCLsEYihd9cSYEwSlGERnlyciHUjet7kaQI4DmKNxA/kZewCuQsxLbD6FDxuYSazArG8kqdBFUzHdlimULWaBjmyS5EiaF5rff7w86thDwnmHMLcTDuBzSZalSyCQiV9VSTz9c9PMHt+VYQIMthbzEQg0Z7qAXUSA1gDXiK+3WsaoeAvBJpkWfFXTCV1djFlOedBtDoGIPSS4S22eB5ka9T6ih5gsxtnno2Gbhw1P3KzrCgwwhO47dms7O4cFfcIIOzINWh66VYXh3RO/8XdjTojcPaI5pGAr5jyj4AEpBytT84wAzMy5kbu9XVXNXe8tvKy92+cohJnLB0rCsl2SXdtXe9nZ8WPu+d8q0IiCLrINtIro1SOyU7NYv++VRnU6sPYdsNkKOZDZeygs/UfvGqh27RvYnOLn5vt9spV8pSM3s4cnzp9Kj+CgKY5nNlR3s326MZL7FOWb1WuYDOUBb0FA6MfHx184ynP5YXG2hNOSURh7j51uB74asBJjjPf6JHJvYFQJKU4GAlZgIxED7tKV2URxhpP4YnQxJZbP1WAZmOgJPzXjI5WFw4DnfkIT5PhKM1IsPbnJJN5vJ6pGhSZS0uBMq0NUrXJVhp18Y0qa1+zlKA8lu/O1KiuS5xgGmOF4l/gHG/Jr6GFdZQUhXa8CxDGtaztlF9QmgRySFOcFmcZYmk7tvDROecTj7F1CQr+jrNSsGpLZq2ufKOIuNnoMLI4MK/chyHF7+3ptPcQK1ZaXWkg7nyiWw7MDmSUU7uxfCYIEf1wGHOsu+pTtqg/BumBrCqiltBXW2dBaYIE6gPEKYR4Y58n0mdTft22JWQ7yRblCSAh2Qi6d7uuRtVT1UHFNLsZs21WcFY0/iotx6kPuTt2+QOjPkT13pL0tH5Nbc6MZdK+FzI3v4Kv4duw6S6WzZbTU2Ob20wzmDsq2mAJnHMNe65fvojCxY//A+oVjwR8Mi/CLx9TePDHqwA9HtlbFzPUvBv9jiTx5NSv2NbiVVmP0gOEEeuCq3E7SqWxginbIdHF+jLIrGC1fnfJmak13k0cGX8UTaymA3D38DQIwXFMA0jWT0oS+Rq6KrY4AuZ'];_0x1910=function(){return _0x1718d9;};return _0x1910();}var objc=JSON['parse']($response[_0x1337da(0xc2)]);function _0x4fbc(_0x18ae48,_0x129ec1){var _0x191062=_0x1910();return _0x4fbc=function(_0x4fbc0c,_0x3c2d61){_0x4fbc0c=_0x4fbc0c-0xb7;var _0x55b2a0=_0x191062[_0x4fbc0c];return _0x55b2a0;},_0x4fbc(_0x18ae48,_0x129ec1);}objc['data']=_0x1337da(0xc3),$done({'body':JSON[_0x1337da(0xbd)](objc)});
