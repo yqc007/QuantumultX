a9e839af475c654d67d2b276dcdd3ab0fb9e7e7e
Hausd0rff <s2232094232@gmail.com>
2023-07-03 19:39:42 +0800

    Create OneClickCutout

---
+++ OneClickCoutVipCrack.js
@@ -0,0 +1,18 @@
+/******************************
+脚本功能：一键抠图解锁永久会员
+应用版本：1.5.0
+应用下载：http://t.cn/A6pdVNzJ
+脚本作者：Hausd0rff
+更新时间：2023-06-29
+脚本发布：https://t.me/yqc_123
+使用声明：⚠️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+# > 一键抠图解锁永久会员
+^https?:\/\/api\.mattingm\.com\/receipt_api\/user\/info url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/OneClickCoutVipCrack.js
+[mitm] 
+hostname = api.mattingm.com
+
+*******************************/
+
+var _0x48a4c5=_0x3fa5;(function(_0x3a7b7b,_0x279e36){var _0x1bdc82=_0x3fa5,_0x5ae22d=_0x3a7b7b();while(!![]){try{var _0x45e488=-parseInt(_0x1bdc82(0x15d))/0x1+parseInt(_0x1bdc82(0x15f))/0x2*(-parseInt(_0x1bdc82(0x167))/0x3)+parseInt(_0x1bdc82(0x15e))/0x4+parseInt(_0x1bdc82(0x15b))/0x5*(-parseInt(_0x1bdc82(0x166))/0x6)+-parseInt(_0x1bdc82(0x163))/0x7*(parseInt(_0x1bdc82(0x165))/0x8)+-parseInt(_0x1bdc82(0x155))/0x9*(-parseInt(_0x1bdc82(0x156))/0xa)+parseInt(_0x1bdc82(0x157))/0xb*(parseInt(_0x1bdc82(0x164))/0xc);if(_0x45e488===_0x279e36)break;else _0x5ae22d['push'](_0x5ae22d['shift']());}catch(_0x107c37){_0x5ae22d['push'](_0x5ae22d['shift']());}}}(_0x2882,0x33895));function _0x2882(){var _0x546144=['2EknrSU','data','expire_at','2999-12-28\x2000:00:00','35zMukOu','9452952dWRDKR','300728XBCXDI','12VmusMk','298092vnhVEW','1515321dGhIWi','10PpxMgw','11ZYanEp','parse','state','user_id','920030mDVaeL','stringify','271304GhLUcg','726452HwFROx'];_0x2882=function(){return _0x546144;};return _0x2882();}function _0x3fa5(_0x2760e1,_0x3f85c3){var _0x288257=_0x2882();return _0x3fa5=function(_0x3fa5fa,_0x3e587a){_0x3fa5fa=_0x3fa5fa-0x155;var _0x2d8863=_0x288257[_0x3fa5fa];return _0x2d8863;},_0x3fa5(_0x2760e1,_0x3f85c3);}var body=$response['body'],hausd0rff=JSON[_0x48a4c5(0x158)](body);hausd0rff[_0x48a4c5(0x160)][_0x48a4c5(0x159)]=0x1,hausd0rff[_0x48a4c5(0x160)][_0x48a4c5(0x15a)]=0xa2c2a,hausd0rff[_0x48a4c5(0x160)][_0x48a4c5(0x161)]=_0x48a4c5(0x162),$done({'body':JSON[_0x48a4c5(0x15c)](hausd0rff)});
