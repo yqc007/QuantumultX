da2440546d236b25e424c4d21bb9a6dd6a33ea27
Hausd0rff <s2232094232@gmail.com>
2023-07-10 11:31:32 +0800

    Create MateTranslate

---
+++ MateTranslateProCrack.js
@@ -0,0 +1,18 @@
+/******************************
+脚本功能：Mate 翻译解锁永久订阅
+应用版本：7.2.0
+应用下载：http://t.cn/A6074oST
+脚本作者：Hausd0rff
+更新时间：2023-06-28
+脚本发布：https://t.me/yqc_123
+使用声明：⚠️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+# > Mate 翻译解锁永久订阅
+^https?:\/\/asia\.gikken\.co\/matesync\/subscription$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/MateTranslateProCrack.js
+[mitm] 
+hostname = asia.gikken.co
+
+*******************************/
+
+function _0xb3b8(){var _0x4168ac=['1356920SnTXpr','981oDGQTS','1411035PKFTym','stringify','80824lNCtTr','560100gsMYEY','757866WXqMlB','400759sKCGbI','722325xQVKZM','7MCUiLs'];_0xb3b8=function(){return _0x4168ac;};return _0xb3b8();}var _0x316e77=_0x13cb;function _0x13cb(_0x114131,_0x299468){var _0xb3b8ed=_0xb3b8();return _0x13cb=function(_0x13cbbb,_0x46369d){_0x13cbbb=_0x13cbbb-0x189;var _0x3c55fd=_0xb3b8ed[_0x13cbbb];return _0x3c55fd;},_0x13cb(_0x114131,_0x299468);}(function(_0xfc9cbc,_0x21c923){var _0x192ba8=_0x13cb,_0x4dee55=_0xfc9cbc();while(!![]){try{var _0x6fa162=parseInt(_0x192ba8(0x18b))/0x1+parseInt(_0x192ba8(0x18e))/0x2+parseInt(_0x192ba8(0x18c))/0x3+parseInt(_0x192ba8(0x189))/0x4+parseInt(_0x192ba8(0x190))/0x5+-parseInt(_0x192ba8(0x18a))/0x6*(parseInt(_0x192ba8(0x18d))/0x7)+-parseInt(_0x192ba8(0x192))/0x8*(parseInt(_0x192ba8(0x18f))/0x9);if(_0x6fa162===_0x21c923)break;else _0x4dee55['push'](_0x4dee55['shift']());}catch(_0x30c9c4){_0x4dee55['push'](_0x4dee55['shift']());}}}(_0xb3b8,0x7da80),$done({'body':JSON[_0x316e77(0x191)]({'isLifetime':!![],'isSubscriptionVerified':!![],'hasBoughtPaidApp':!![]})}));
