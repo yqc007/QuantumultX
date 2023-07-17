2eda6bd568ceccbcbe781fea6d3cdc1fd967c6c2
Hausd0rff <s2232094232@gmail.com>
2023-07-17 16:24:59 +0800

    Create MeiTuAction

---
+++ ActionForeverVipCrack.js
@@ -0,0 +1,18 @@
+/******************************
+脚本功能：开拍解锁永久会员
+应用版本：1.0.90
+应用下载：http://t.cn/A60bq2t6
+脚本作者：Hausd0rff
+更新时间：2023-07-12
+脚本发布：https://t.me/yqc_123
+使用声明：⚠️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+# > 开拍解锁永久会员
+^https?:\/\/api-sub\.meitu\.com\/v2\/user\/vip_info_by_group\.json url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/ActionForeverVipCrack.js
+[mitm] 
+hostname = api-sub.meitu.com
+
+*******************************/
+
+var _0x67a266=_0x1038;function _0x1038(_0x532134,_0xb3a2bc){var _0x43b6ea=_0x43b6();return _0x1038=function(_0x10382a,_0x120a2d){_0x10382a=_0x10382a-0x10b;var _0x44f4c0=_0x43b6ea[_0x10382a];return _0x44f4c0;},_0x1038(_0x532134,_0xb3a2bc);}(function(_0x3ee10e,_0x24170d){var _0x171a03=_0x1038,_0x215e97=_0x3ee10e();while(!![]){try{var _0x1f90bd=-parseInt(_0x171a03(0x10b))/0x1+-parseInt(_0x171a03(0x114))/0x2*(parseInt(_0x171a03(0x11a))/0x3)+-parseInt(_0x171a03(0x112))/0x4*(parseInt(_0x171a03(0x111))/0x5)+-parseInt(_0x171a03(0x10d))/0x6*(-parseInt(_0x171a03(0x11f))/0x7)+parseInt(_0x171a03(0x119))/0x8+parseInt(_0x171a03(0x110))/0x9*(-parseInt(_0x171a03(0x10f))/0xa)+-parseInt(_0x171a03(0x11b))/0xb*(-parseInt(_0x171a03(0x10e))/0xc);if(_0x1f90bd===_0x24170d)break;else _0x215e97['push'](_0x215e97['shift']());}catch(_0x311043){_0x215e97['push'](_0x215e97['shift']());}}}(_0x43b6,0xcbed4));var __Oxfe574=['body',_0x67a266(0x116),'data','\u7eed\u671f',_0x67a266(0x115),'',_0x67a266(0x11c),_0x67a266(0x10c),_0x67a266(0x117),'4',_0x67a266(0x120),_0x67a266(0x118),_0x67a266(0x11d),_0x67a266(0x11e),'0',_0x67a266(0x113)],body=$response[__Oxfe574[0x0]],hausd0rff=JSON[__Oxfe574[0x1]](body);hausd0rff[__Oxfe574[0x2]]={'active_sub_type':0x2,'account_type':0x1,'sub_type_name':__Oxfe574[0x3],'active_sub_order_id':__Oxfe574[0x4],'trial_period_invalid_time':__Oxfe574[0x5],'current_order_invalid_time':__Oxfe574[0x6],'active_order_id':__Oxfe574[0x7],'limit_type':0x0,'active_sub_type_name':__Oxfe574[0x3],'use_vip':!![],'have_valid_contract':!![],'derive_type_name':__Oxfe574[0x8],'derive_type':0x1,'in_trial_period':![],'is_vip':!![],'membership':{'id':__Oxfe574[0x9],'display_name':__Oxfe574[0xa],'level':0x1,'level_name':__Oxfe574[0x8]},'active_promotion_status_list':[0x2],'sub_type':0x2,'account_id':__Oxfe574[0xb],'invalid_time':__Oxfe574[0xc],'valid_time':__Oxfe574[0xd],'active_product_id':__Oxfe574[0xe],'active_promotion_status':0x2,'show_renew_flag':!![]},$done({'body':JSON[__Oxfe574[0xf]](hausd0rff)});function _0x43b6(){var _0xbdfef=['\u6c38\u4e45\u4f1a\u5458','1230010086','6224080HaNqNq','152967PsrUmS','440HKQdhk','32495508000000','32495529599000','1569664800000','1180501qhYSbj','\u5f00\u62cd\u4f1a\u5458','1632753iNcjAB','7069961436340181123','36jJCsRa','1107036IeMJMc','24940oGydob','1287zBtaVz','456645qaCmRU','56gjNcWT','stringify','54bIraKj','7069961436604422668','parse'];_0x43b6=function(){return _0xbdfef;};return _0x43b6();}
