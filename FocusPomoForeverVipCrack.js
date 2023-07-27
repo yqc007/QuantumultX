07f8eb111a29e96ef71e6ce3f5e326be3b601b8e
Hausd0rff <s2232094232@gmail.com>
2023-07-27 18:55:47 +0800

    Create FocusPomo

---
+++ FocusPomoForeverVipCrack.js
@@ -0,0 +1,18 @@
+/******************************
+脚本功能：我的番茄解锁永久会员
+应用版本：2.10.0
+应用下载：http://t.cn/A60ayD38
+脚本作者：Hausd0rff
+更新时间：2023-07-21
+脚本发布：https://t.me/yqc_123
+使用声明：⚠️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+# > 我的番茄解锁永久会员
+^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/\$RCAnonymousID%3A\w{32}$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/FocusPomoForeverVipCrack.js
+[mitm] 
+hostname = api.revenuecat.com
+
+*******************************/
+
+function _0x352d(){var _0x4adb66=['2023-06-27T10:03:02Z','2023-06-27T10:08:55Z','2023-06-26T23:02:50Z','2018084EyRqxa','150978vrFqJs','10uoWhbf','843018jlZFPz','2023-06-27T10:08:54Z','2734599ekAwQh','3784392ZynVej','350001638433463','11521','4856829FJvUXl','22073872NIIkfV','2999-12-28T09:00:00Z','stringify','https://apps.apple.com/account/subscriptions'];_0x352d=function(){return _0x4adb66;};return _0x352d();}function _0x2f8a(_0x473353,_0x6e67f9){var _0x352dae=_0x352d();return _0x2f8a=function(_0x2f8a50,_0xf371e1){_0x2f8a50=_0x2f8a50-0x197;var _0x5c9b57=_0x352dae[_0x2f8a50];return _0x5c9b57;},_0x2f8a(_0x473353,_0x6e67f9);}var _0x334193=_0x2f8a;(function(_0x24c8c6,_0x1a3cad){var _0x834efd=_0x2f8a,_0x5a2348=_0x24c8c6();while(!![]){try{var _0x3e1f95=parseInt(_0x834efd(0x19b))/0x1+-parseInt(_0x834efd(0x19d))/0x2+-parseInt(_0x834efd(0x1a3))/0x3+-parseInt(_0x834efd(0x19a))/0x4*(-parseInt(_0x834efd(0x19c))/0x5)+-parseInt(_0x834efd(0x1a0))/0x6+-parseInt(_0x834efd(0x19f))/0x7+parseInt(_0x834efd(0x1a4))/0x8;if(_0x3e1f95===_0x1a3cad)break;else _0x5a2348['push'](_0x5a2348['shift']());}catch(_0x102411){_0x5a2348['push'](_0x5a2348['shift']());}}}(_0x352d,0xd1545),$done({'body':JSON[_0x334193(0x1a6)]({'request_date_ms':0x18996da29c9,'request_date':'2023-06-27T10:17:07Z','subscriber':{'non_subscriptions':{},'first_seen':_0x334193(0x197),'original_application_version':_0x334193(0x1a2),'other_purchases':{},'management_url':_0x334193(0x1a7),'subscriptions':{'com.MINE.PomodoroTimer.plus.lifetime':{'original_purchase_date':_0x334193(0x198),'expires_date':'2999-12-28T09:00:00Z','is_sandbox':![],'refunded_at':null,'store_transaction_id':_0x334193(0x1a1),'unsubscribe_detected_at':null,'grace_period_expires_date':null,'period_type':'plus','purchase_date':_0x334193(0x19e),'billing_issues_detected_at':null,'ownership_type':'PURCHASED','store':'app_store','auto_resume_date':null}},'entitlements':{'Lifetime':{'grace_period_expires_date':null,'purchase_date':_0x334193(0x19e),'product_identifier':'com.MINE.PomodoroTimer.plus.lifetime','expires_date':_0x334193(0x1a5)},'Plus':{'grace_period_expires_date':null,'purchase_date':'2023-06-27T10:08:54Z','product_identifier':'com.MINE.PomodoroTimer.plus.lifetime','expires_date':'2999-12-28T09:00:00Z'}},'original_purchase_date':_0x334193(0x199),'original_app_user_id':'$RCAnonymousID:703ef974c072411c8df2e0a6c94f4yqc','last_seen':_0x334193(0x197)}})}));
