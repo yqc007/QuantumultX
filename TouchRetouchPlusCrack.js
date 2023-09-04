8ee9c7ca2a2c25c724c68760b8135c1187f03c39
Hausd0rff <s2232094232@gmail.com>
2023-09-04 17:28:02 +0800

    Create TouchRetouch

---
+++ TouchRetouchPlusCrack.js
@@ -0,0 +1,22 @@
+/******************************
+
+脚本功能：TouchRetouch 解锁永久 Plus
+应用版本：5.1.10
+应用下载：http://t.cn/A6OcWI6B
+脚本作者：Hausd0rff
+更新时间：2023-09-02
+脚本发布：https://t.me/yqc_123
+使用声明：⚠️仅供学习交流, 🈲️商业用途
+
+*******************************
+
+[rewrite_local]
+# > TouchRetouch 解锁永久 Plus
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/\$RCAnonymousID%3A\w{32}|receipts)$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/TouchRetouchPlusCrack.js
+
+[mitm] 
+hostname = api.revenuecat.com
+
+*******************************/
+
+function _0x1387(_0xb762c4,_0x4d3045){var _0x25b607=_0x25b6();return _0x1387=function(_0x1387e9,_0xa74c57){_0x1387e9=_0x1387e9-0x103;var _0x4fbcaa=_0x25b607[_0x1387e9];return _0x4fbcaa;},_0x1387(_0xb762c4,_0x4d3045);}var _0x22e58a=_0x1387;(function(_0x4a5737,_0x200150){var _0x2ab898=_0x1387,_0x25f56c=_0x4a5737();while(!![]){try{var _0x427623=parseInt(_0x2ab898(0x107))/0x1*(-parseInt(_0x2ab898(0x10d))/0x2)+-parseInt(_0x2ab898(0x106))/0x3*(parseInt(_0x2ab898(0x115))/0x4)+-parseInt(_0x2ab898(0x10c))/0x5*(parseInt(_0x2ab898(0x111))/0x6)+parseInt(_0x2ab898(0x110))/0x7*(parseInt(_0x2ab898(0x113))/0x8)+-parseInt(_0x2ab898(0x112))/0x9+parseInt(_0x2ab898(0x117))/0xa+parseInt(_0x2ab898(0x119))/0xb*(parseInt(_0x2ab898(0x10a))/0xc);if(_0x427623===_0x200150)break;else _0x25f56c['push'](_0x25f56c['shift']());}catch(_0xc14bfd){_0x25f56c['push'](_0x25f56c['shift']());}}}(_0x25b6,0xbfe88));function _0x25b6(){var _0xe53b6d=['9010856tJwfUz','PURCHASED','4kaJUBq','app_store','12107680YntGzm','entitlements','8520721xhHAnm','https:\/\/t.me\/yqc_123','tr5_yearlysubsc_30_and_20_dlrs','350001681261518','subscriber','parse','539769nZSYCb','51559ILSBQA','2999-12-28T09:00:00Z','2022-06-06T06:00:00Z','24rfPvOE','stringify','354950bmEwdc','20pcNehX','management_url','premium','7TpRNyl','132xfjUVy','7586982JEtPyb'];_0x25b6=function(){return _0xe53b6d;};return _0x25b6();}var body=$response['body'],objc=JSON[_0x22e58a(0x105)](body),subs={'tr5_yearlysubsc_30_and_20_dlrs':{'original_purchase_date':'2022-06-06T06:00:00Z','expires_date':_0x22e58a(0x108),'is_sandbox':![],'refunded_at':null,'store_transaction_id':_0x22e58a(0x103),'unsubscribe_detected_at':null,'grace_period_expires_date':null,'period_type':_0x22e58a(0x10f),'purchase_date':_0x22e58a(0x109),'billing_issues_detected_at':null,'ownership_type':_0x22e58a(0x114),'store':_0x22e58a(0x116),'auto_resume_date':null}},enti={'premium':{'grace_period_expires_date':null,'purchase_date':_0x22e58a(0x109),'product_identifier':_0x22e58a(0x11b),'expires_date':_0x22e58a(0x108)}};objc[_0x22e58a(0x104)][_0x22e58a(0x10e)]=_0x22e58a(0x11a),objc[_0x22e58a(0x104)]['subscriptions']=subs,objc[_0x22e58a(0x104)][_0x22e58a(0x118)]=enti,$done({'body':JSON[_0x22e58a(0x10b)](objc)});
