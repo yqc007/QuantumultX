c1db6273cc20bea54ea355a77dbdf23f97d5680e
Hausd0rff <s2232094232@gmail.com>
2023-09-05 17:01:15 +0800

    Update XinYuLibrary

--- XinYuLibraryProCrack.js
+++ XinYuLibraryProCrack.js
@@ -1,35 +1,22 @@
-/*
- *
- *
-脚本功能：新语听书解锁会员
-软件版本：4.2.9
-下载地址：http://t.cn/A6xkFpuX
+/******************************
+
+脚本功能：新语听书解锁永久会员
+应用版本：4.2.24
+应用下载：http://t.cn/A6xkFpuX
 脚本作者：Hausd0rff
-更新时间：2021-11-15
-电报频道：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_777
-使用声明：⚠️此脚本仅供学习与交流，
-        请勿转载与贩卖！⚠️⚠️⚠️
+更新时间：2023-08-27
+脚本发布：https://t.me/yqc_123
+使用声明：⚠️仅供学习交流, 🈲️商业用途
+
 *******************************
 
 [rewrite_local]
-
-# > 新语听书解锁会员
+# > 新语听书解锁永久会员
 ^https?:\/\/i\.xinyulib\.com\.cn\/api\/querytoken.+ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/XinYuLibraryProCrack.js
 
 [mitm] 
-
 hostname = i.xinyulib.com.cn
-*
-*
-*/
 
+*******************************/
 
-var objc = JSON.parse($response.body);
-objc.data["vipstartTime"] = "2020-09-28";
-objc.data["vipendtime"] = "2999-09-28";
-objc.data["trueName"] = "\u6D65\u8F7B\u5C18\uD835\uDC36\uD835\uDC5F\uD835\uDC4E\uD835\uDC50\uD835\uDC58";
-objc.data["headImg"] = "https:\/\/thirdqq.qlogo.cn\/g?b=oidb&k=iaODNibZcQQTN0QPGShfkKIw&s=640";
-$done({
-    body : JSON.stringify(objc)
-});
+var _0xbda2c8=_0x93dc;(function(_0x1b4367,_0x4dfcf8){var _0x14fccc=_0x93dc,_0x43d09c=_0x1b4367();while(!![]){try{var _0x4b70de=parseInt(_0x14fccc(0x1f4))/0x1+-parseInt(_0x14fccc(0x1f0))/0x2*(-parseInt(_0x14fccc(0x1fe))/0x3)+parseInt(_0x14fccc(0x1f7))/0x4*(-parseInt(_0x14fccc(0x1fc))/0x5)+parseInt(_0x14fccc(0x1f8))/0x6+parseInt(_0x14fccc(0x1f2))/0x7+-parseInt(_0x14fccc(0x1f3))/0x8*(parseInt(_0x14fccc(0x1f5))/0x9)+-parseInt(_0x14fccc(0x1f1))/0xa;if(_0x4b70de===_0x4dfcf8)break;else _0x43d09c['push'](_0x43d09c['shift']());}catch(_0x3ae9fc){_0x43d09c['push'](_0x43d09c['shift']());}}}(_0x4e7a,0x61452));function _0x4e7a(){var _0x19dd18=['15GEIwSN','vipendtime','162273apvSeh','stringify','data','2999-09-28','4ohmuqg','4169160FDNYhs','990738tdkbkL','16siBXDg','495205wKbcSX','1067922iJOxSs','trueName','384760acbHLj','3577794SsRoYv','headImg','body','vipstartTime'];_0x4e7a=function(){return _0x19dd18;};return _0x4e7a();}function _0x93dc(_0x38df6c,_0x4ca135){var _0x4e7a56=_0x4e7a();return _0x93dc=function(_0x93dc29,_0x96edb8){_0x93dc29=_0x93dc29-0x1ee;var _0x4c325c=_0x4e7a56[_0x93dc29];return _0x4c325c;},_0x93dc(_0x38df6c,_0x4ca135);}var objc=JSON['parse']($response[_0xbda2c8(0x1fa)]);objc[_0xbda2c8(0x1ee)][_0xbda2c8(0x1fb)]='2020-09-28',objc[_0xbda2c8(0x1ee)][_0xbda2c8(0x1fd)]=_0xbda2c8(0x1ef),objc[_0xbda2c8(0x1ee)][_0xbda2c8(0x1f6)]='\u6D65\u8F7B\u5C18\uD835\uDC36\uD835\uDC5F\uD835\uDC4E\uD835\uDC50\uD835\uDC58',objc[_0xbda2c8(0x1ee)][_0xbda2c8(0x1f9)]='https:\/\/thirdqq.qlogo.cn\/g?b=oidb&k=iaODNibZcQQTN0QPGShfkKIw',$done({'body':JSON[_0xbda2c8(0x1ff)](objc)});
