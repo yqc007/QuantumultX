c0cb1861c34440fb1d0b337345fadf479afdf6fa
Hausd0rff <s2232094232@gmail.com>
2023-10-30 16:37:39 +0800

    Create TimeNote

---
+++ TimeNoteForeverVipCrack.js
@@ -0,0 +1,22 @@
+/******************************
+
+脚本功能：指尖笔记解锁永久会员
+应用版本：3.1.0
+应用下载：http://t.cn/A6WZjujw
+脚本作者：Hausd0rff
+更新时间：2023-09-29
+脚本发布：https://t.me/yqc_123
+使用声明：⚠️仅供学习交流, 🈲️商业用途
+
+*******************************
+
+[rewrite_local]
+# > 指尖笔记解锁永久会员
+^https?:\/\/data\.ufbae\.com\/User\/GetUser url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/TimeNoteForeverVipCrack.js
+
+[mitm] 
+hostname = data.ufbae.com
+
+*******************************/
+
+var _0x1b3db7=_0x21b9;function _0x21b9(_0x5d506c,_0x3ae88c){var _0x2c67a3=_0x2c67();return _0x21b9=function(_0x21b937,_0x231577){_0x21b937=_0x21b937-0x1dc;var _0x297c98=_0x2c67a3[_0x21b937];return _0x297c98;},_0x21b9(_0x5d506c,_0x3ae88c);}(function(_0x1f6a2a,_0x56bee5){var _0x3ad299=_0x21b9,_0x266017=_0x1f6a2a();while(!![]){try{var _0x109641=-parseInt(_0x3ad299(0x1e8))/0x1*(parseInt(_0x3ad299(0x1ed))/0x2)+-parseInt(_0x3ad299(0x1e3))/0x3+parseInt(_0x3ad299(0x1e4))/0x4*(parseInt(_0x3ad299(0x1ec))/0x5)+-parseInt(_0x3ad299(0x1e9))/0x6+parseInt(_0x3ad299(0x1ee))/0x7+-parseInt(_0x3ad299(0x1e0))/0x8+parseInt(_0x3ad299(0x1e2))/0x9*(parseInt(_0x3ad299(0x1de))/0xa);if(_0x109641===_0x56bee5)break;else _0x266017['push'](_0x266017['shift']());}catch(_0x2e37e3){_0x266017['push'](_0x266017['shift']());}}}(_0x2c67,0xbd7cd));var body=$response[_0x1b3db7(0x1ea)],objc=JSON[_0x1b3db7(0x1e1)](body);function _0x2c67(){var _0x3768c8=['body','https://t.me/yqc_123','1784945JUgcJX','6KOKqoX','8026340WEflMp','nickName','snsPlatform','10nfmkPB','memBeginTs','6341024cChjOP','parse','6065253vpbITy','1281801cBUMTu','16fBkHgE','https://zhijiannote.oss-cn-beijing.aliyuncs.com/cc3a41f83f23fef1ec0dbd2d0e2d83b8/header1695805205.4716911_file.png','Hausd0rff','photo','140526YyEnJs','4985274LzOPTW'];_0x2c67=function(){return _0x3768c8;};return _0x2c67();}objc['vip']=0x1,objc['christmasVIP']=0x1,objc[_0x1b3db7(0x1df)]=0x174d4279d00,objc['memEndTs']=0x1d8df3360d00,objc[_0x1b3db7(0x1dc)]=_0x1b3db7(0x1e6),objc[_0x1b3db7(0x1e7)]=_0x1b3db7(0x1e5),objc[_0x1b3db7(0x1dd)]=_0x1b3db7(0x1eb),$done({'body':JSON['stringify'](objc)});
