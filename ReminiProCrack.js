08eada498e940f8ec190fb4233a781d1628abc80
Hausd0rff <yqcrack@gmail.com>
2024-05-08 17:33:32 +0800

    Create ReminiProCrack

---
+++ ReminiProCrack.js
@@ -0,0 +1,18 @@
+/******************************************
+ * @Name 𝑅𝑒𝑚𝑖𝑛𝑖
+ * @Description 𝑈𝑛𝑙𝑜𝑐𝑘 𝑃𝑟𝑜 𝑆𝑢𝑏𝑠𝑐𝑟𝑖𝑝𝑡𝑖𝑜𝑛
+ * @Channel https://t.me/yqc_123
+ * @Feedback https://t.me/yqc_777
+ * @Author 𝐻𝑎𝑢𝑠𝑑𝑜𝑟𝑓𝑓
+ * @Update 20240508
+ * @Link http://t.cn/A6H2Hx43
+ * @Version 2.10.28
+ ******************************************
+[rewrite_local]
+# > 𝑅𝑒𝑚𝑖𝑛𝑖 𝐶𝑟𝑎𝑐𝑘 𝑃𝑟𝑜
+^https?:\/\/remini\.oracle\.bendingspoonsapps\.com\/v2\/users\/setup$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/ReminiProCrack.js
+[mitm] 
+hostname = remini.oracle.bendingspoonsapps.com
+******************************************/
+
+function _0x1a7f(_0x3e618c,_0x1223f8){var _0x11854f=_0x1185();return _0x1a7f=function(_0x1a7f3b,_0x209608){_0x1a7f3b=_0x1a7f3b-0x1e9;var _0x2bfcff=_0x11854f[_0x1a7f3b];return _0x2bfcff;},_0x1a7f(_0x3e618c,_0x1223f8);}var _0x57f93c=_0x1a7f;(function(_0x24577f,_0x21ace9){var _0x4a66b0=_0x1a7f,_0x37bb35=_0x24577f();while(!![]){try{var _0xda04c5=-parseInt(_0x4a66b0(0x1f6))/0x1*(parseInt(_0x4a66b0(0x1ed))/0x2)+parseInt(_0x4a66b0(0x1f3))/0x3*(parseInt(_0x4a66b0(0x1ea))/0x4)+-parseInt(_0x4a66b0(0x1f1))/0x5+parseInt(_0x4a66b0(0x1eb))/0x6*(parseInt(_0x4a66b0(0x1fa))/0x7)+-parseInt(_0x4a66b0(0x1ef))/0x8+-parseInt(_0x4a66b0(0x1f4))/0x9*(-parseInt(_0x4a66b0(0x1f8))/0xa)+parseInt(_0x4a66b0(0x1f7))/0xb;if(_0xda04c5===_0x21ace9)break;else _0x37bb35['push'](_0x37bb35['shift']());}catch(_0x13c550){_0x37bb35['push'](_0x37bb35['shift']());}}}(_0x1185,0x4dcf3));var body=$response[_0x57f93c(0x1f9)],hausd0rff=JSON[_0x57f93c(0x1f2)](body);function _0x1185(){var _0x552172=['417NLTGlq','358011njnjpL','active_subscriptions_ids','162557wAAGNT','6923367XatFPc','20XFjyTt','body','329AlITSj','active_subscriptions','1844TAqXnS','52224dxiZag','apple','2DRRBAZ','stringify','4089784sycrYL','com.bigwinepot.nwdn.international.1y_p99_99_pro','948175XQqMLM','parse'];_0x1185=function(){return _0x552172;};return _0x1185();}hausd0rff['me'][_0x57f93c(0x1f5)]=[_0x57f93c(0x1f0)],hausd0rff['me'][_0x57f93c(0x1e9)]=[{'id':_0x57f93c(0x1f0),'vendor':_0x57f93c(0x1ec),'timestamp':0x63338e80}],$done({'body':JSON[_0x57f93c(0x1ee)](hausd0rff)});
